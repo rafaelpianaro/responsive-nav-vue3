8266fcda2361d55c03c769cd5b149088a4cfdb76
Rafael Pianaro <rafael_pianaro@econeteditora.com.br>
2022-03-17 13:05:35 -0300

    Navigation component into App.Vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="app">
+    <Navigation />
     <router-view />
   </div>
 </template>
 
 <script>
+import Navigation from "./components/Navigation.vue"
 export default {
   name: "App",
+  components: {
+    Navigation,
+  },
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wgth@400;500&display=swap");
 * {
   padding: 0;
   margin: 0;
