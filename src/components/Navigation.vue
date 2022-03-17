a93478a6a5fd4eada990d08a305c39ce38308d66
Rafael Pianaro <rafael_pianaro@econeteditora.com.br>
2022-03-17 12:55:37 -0300

    *Navigation.vue *router-link

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <header :class="{ 'scrolled-nav':scrollPosition }">
+      <nav>
+          <div class="branding">
+              <img src="@/assets/Logo.png" alt="" >
+          </div>
+          <ul v-show="!mobile" class="navigation">
+              <li><router-link class="link" :to="{name: 'Home'}">Home</router-link></li>
+              <li><router-link class="link" :to="{name: ''}">About</router-link></li>
+              <li><router-link class="link" :to="{name: ''}">Portfolio</router-link></li>
+              <li><router-link class="link" :to="{name: ''}">Contact</router-link></li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
