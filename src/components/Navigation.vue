eab055f6754602ab03bca891c3749f1dd1df9451
Rafael Pianaro <rafael_pianaro@econeteditora.com.br>
2022-03-17 13:00:33 -0300

    *mobile-nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -10,6 +10,17 @@
               <li><router-link class="link" :to="{name: ''}">Portfolio</router-link></li>
               <li><router-link class="link" :to="{name: ''}">Contact</router-link></li>
           </ul>
+          <div class="icon">
+              <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars" :class="{ 'icon-active':mobileNav }"></i>
+          </div>
+          <transition name="mobile-nav">
+            <ul v-show="mobileNav" class="dropdown-nav">
+              <li><router-link class="link" :to="{name: 'Home'}">Home</router-link></li>
+              <li><router-link class="link" :to="{name: ''}">About</router-link></li>
+              <li><router-link class="link" :to="{name: ''}">Portfolio</router-link></li>
+              <li><router-link class="link" :to="{name: ''}">Contact</router-link></li>
+            </ul>
+          </transition>
       </nav>
   </header>
 </template>
