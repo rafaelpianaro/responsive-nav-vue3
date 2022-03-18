68226f60991ab43f677eac691e6a55f80801b664
Rafael Pianaro <rafael_pianaro@econeteditora.com.br>
2022-03-18 13:30:14 -0300

    styled Navigaton.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -31,6 +31,66 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+
+header {
+  background-color: rgba(0,0,0,0.8);
+  z-index: 99;
+  width: 100%;
+  position: fixed;
+  transition: 0.5s ease all;
+  color: #fff;
+
+  nav {
+    display: flex;
+    flex-direction: row;
+    padding: 12px 0;
+    transition: .5s ease all;
+    width: 90%;
+    margin: 0 auto;
+    @media(min-width: 1140px) {
+      max-width: 1140px;
+    }
+
+    ul,
+    .link {
+      font-weight: 500;
+      color: #fff;
+      list-style: nome;
+      text-decoration: nome;
+    }
+
+    li {
+      text-transform: uppercase;
+      padding: 16px;
+      margin-left: 16px;
+    }
+
+    .link {
+      font-size: 14px;
+      transition: 0.5s ease all;
+      padding-bottom: 4px;
+      border-bottom: 1px solid transparent;
+
+      &:hover {
+        color: #00afea;
+        border-color: #00afea;
+      }
+    }
+
+    .branding {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 50px;
+        transition: 0.5s ease all;
+      }
+    }
+
+
+  }
+
+}
 
 </style>
