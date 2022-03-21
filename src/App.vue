d625527e6fde5cb8383760e7fc2fe05e62e28272
Rafael <rafaelpianaro@gmail.com>
2022-03-20 21:23:58 -0300

    fix @import

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Raleway:wgth@400;500&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@400;500&display=swap");
 * {
   padding: 0;
   margin: 0;
